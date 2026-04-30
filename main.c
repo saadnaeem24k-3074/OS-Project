32888fcd33904a273990d877d5e382ea69a79097
Saad Khan <Saadcs8275@gmail.com>
2026-04-30 23:56:05 +0500

    main

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#define _POSIX_C_SOURCE 199309L
+#include "traffic.h"
+#include "raylib.h"
+#include<stdio.h>
+#include<string.h>
 
-int main(){
-    printf("Hello World");
-    return 0;
+
+const char *DIR[]={"North","Eash","South","West"};
+
+static Color sig_color(int sig){
+    if(sig== SIG_GREEN) return GREEN;
+    if(sig== SIG_YELLOW) return YELLOW;
+    return RED;
 }
